7983876a5398df4c0969d3ba5aed8d389276b93f
prakash <prakashranjan993@gmail.com>
2017-04-09 16:38:19 +0530

    globals file

---
+++ globals.php
@@ -0,0 +1,24 @@
+<?php
+include_once"menu.php";
+include_once"juery.js";
+
+$db_host="localhost";
+$db_user="root";
+$db_pwd="";
+$db_name="new";
+
+/*------------connect.php---------------*/
+
+$conn=mysqli_connect($db_host, $db_user, $db_pwd,$db_name) or die("Could not connect Database") ;
+//mysql_select_db($db_name) or die("Could not select Database");
+
+/*------------------variables.php-----------------------*/
+ 
+ foreach($_REQUEST as $variable => $value)
+ {
+	 if(is_array($value))
+		 $$variable=$value;
+	 else
+		 $$variable =trim(strip_tags($value));
+ }
+?>
